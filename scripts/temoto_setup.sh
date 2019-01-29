2cf377034386ec4bc053a22a5248ca106c97f4c4
RValner <valnerrobert@gmail.com>
2019-01-29 17:13:40 +0200

    Setup script update

--- scripts/temoto_setup.sh
+++ scripts/temoto_setup.sh
@@ -8,6 +8,7 @@ NL="\n"
 RESET="\e[39m\e[0m"
 
 ROS_VERSION="kinetic"
+NEW_STUFF_INSTALLED=0
 
 # Usage: find_install_from_source <package_name> <git_repo_uri> 
 find_install_from_source () {
@@ -24,6 +25,7 @@ find_install_from_source () {
     # Clone the rviz_plugin_manager package
     echo -e $RESET$GREEN"Cloning the" $PACKAGE_NAME "package to"$BOLD $CW_DIR/$SUBFOLDER $RESET
     git clone $PACKAGE_PATH
+    NEW_STUFF_INSTALLED=1
   fi 
 }
 
@@ -40,6 +42,7 @@ find_install_from_apt () {
     # Clone the rviz_plugin_manager package
     echo -e $RESET$GREEN"Installing the" $PACKAGE_NAME $RESET
     sudo apt install $PACKAGE_NAME
+    NEW_STUFF_INSTALLED=1
   fi 
 }
 
@@ -71,6 +74,34 @@ fi
 PREV_DIR=$(pwd)
 cd $CW_DIR/$SUBFOLDER
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                  Install TeMoto dependencies
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+echo -e $NL"TeMoto dependencies:"
+
+find_install_from_source rviz_plugin_manager https://github.com/UTNuclearRoboticsPublic/rviz_plugin_manager
+find_install_from_source human_msgs https://github.com/ut-ims-robotics/human_msgs
+find_install_from_source file_template_parser https://github.com/ut-ims-robotics/file_template_parser
+find_install_from_apt ros-$ROS_VERSION-moveit-ros-planning-interface
+find_install_from_apt ros-$ROS_VERSION-tf2-geometry-msgs
+
+# Install Intel TBB
+find_install_from_apt libtbb2
+find_install_from_apt libtbb-dev
+
+# Install TinyXML
+find_install_from_apt libtinyxml-dev
+find_install_from_apt libtinyxml2-2v5
+find_install_from_apt libtinyxml2-dev
+find_install_from_apt libtinyxml2.6.2v5
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                  Install TeMoto subsystems
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+echo -e $NL"TeMoto subsystems:"
+
 # Open the subsystems file
 TEMOTO_SUBSYS_FILE=$(rospack find temoto)/scripts/temoto_subsystems.txt
 SUBSYSTEM_NAMES=$(cat  $TEMOTO_SUBSYS_FILE |tr "\n" " ")
@@ -84,5 +115,33 @@ do
   find_install_from_source $subsys_name https://github.com/temoto-telerobotics/$subsys_name.git
 done
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                  Export TeMoto aliases
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ -z $TEMOTO_ALIASES_SET ]]; then
+  echo -e -n $NL"Export TeMoto script shortcuts? (y/n)" $RESET
+  read -p " " -n 1 -r
+  echo
+
+  # If the user replied "y" or "Y"
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "" >> ~/.bashrc
+    echo "# TeMoto aliases" >> ~/.bashrc
+    echo "alias temoto_pull='bash $(rospack find temoto)/scripts/temoto_pull.sh'" >> ~/.bashrc
+    echo "alias temoto_push='bash $(rospack find temoto)/scripts/temoto_push.sh'" >> ~/.bashrc
+    echo "export TEMOTO_ALIASES_SET=1" >> ~/.bashrc
+    echo -e $YELLOW"NB! The aliases are effective after you have sourced the '.bashrc' or opened a new terminal"$RESET
+  else
+    echo -n
+  fi
+fi
+
 cd $PREV_DIR
-echo -e $NL"Dependencies are installed, you are good to go."
+
+if [[ $NEW_STUFF_INSTALLED == 1 ]]; then
+  echo -e $NL"TeMoto packages are installed,$BOLD run catkin make."$RESET
+  echo -e $YELLOW"NB! If"$BOLD "temoto_nlp"$RESET$YELLOW "package was just installed, then make sure that you have run 'temoto_nlp/scripts/install_meta.sh' before running catkin_make"
+else
+  echo -e $NL"TeMoto packages are installed."
+fi
