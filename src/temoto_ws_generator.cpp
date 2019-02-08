c3a68f9aacd7fda09b7dc393b3d23cadf3692d01
RValner <valnerrobert@gmail.com>
2019-02-08 12:50:18 +0200

    Added nlp_launch template and fixed action sourcing bugs

--- src/temoto_ws_generator.cpp
+++ src/temoto_ws_generator.cpp
@@ -18,14 +18,14 @@ int main(int argc, char **argv)
   const std::string temoto_ws_path = base_path + "/../../" + temoto_ws_name + "/";
   const std::string temoto_ws_package_path = temoto_ws_path + temoto_ws_name + "/";
 
-  // Import the CMakeLists template
+  /*
+   * IMPORT THE TEMPLATES
+   */ 
   tp::TemplateContainer t_cmakelists = tp::TemplateContainer(base_path + "/templates/temoto_ws_cmakelists.xml");
-
-  // Import the package.xml template
   tp::TemplateContainer t_packagexml = tp::TemplateContainer(base_path + "/templates/temoto_ws_packagexml.xml");
-
-  // Import the action path config template
   tp::TemplateContainer t_action_config = tp::TemplateContainer(base_path + "/templates/temoto_ws_action_config.xml");
+  tp::TemplateContainer t_nlp_launch = tp::TemplateContainer(base_path + "/templates/temoto_ws_nlp_launch.xml");
+  tp::TemplateContainer t_components = tp::TemplateContainer(base_path + "/templates/temoto_ws_components.xml");
 
   /*
    * CREATE TEMOTO WS PACKAGE DIRECTORY STRUCTURE
@@ -33,6 +33,7 @@ int main(int argc, char **argv)
   std::cout << "* Creating package folder structure" << std::endl; 
   boost::filesystem::create_directories(temoto_ws_path + "actions");
   boost::filesystem::create_directories(temoto_ws_package_path + "config");
+  boost::filesystem::create_directories(temoto_ws_package_path + "launch");
 
   /*
    * GENERATE THE CONTENT
@@ -41,6 +42,8 @@ int main(int argc, char **argv)
   t_cmakelists.setArgument("temoto_ws_name", temoto_ws_name);
   t_packagexml.setArgument("temoto_ws_name", temoto_ws_name);
   t_action_config.setArgument("temoto_ws_name", temoto_ws_name);
+  t_nlp_launch.setArgument("temoto_ws_name", temoto_ws_name);
+  t_components.setArgument("temoto_ws_name", temoto_ws_name);
 
   /*
    * SAVE THE CONTENT
@@ -49,6 +52,8 @@ int main(int argc, char **argv)
   t_cmakelists.processAndSaveTemplate(temoto_ws_package_path, "CMakeLists");
   t_packagexml.processAndSaveTemplate(temoto_ws_package_path, "package");
   t_action_config.processAndSaveTemplate(temoto_ws_package_path + "/config/", "action_dst");
+  t_nlp_launch.processAndSaveTemplate(temoto_ws_package_path + "/launch/", "temoto_nlp");
+  t_components.processAndSaveTemplate(temoto_ws_package_path, "components");
 
   std::cout << "* Finished generating a TeMoto workspace '" << temoto_ws_name 
             << "' to " << temoto_ws_path << std::endl;
