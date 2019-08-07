8b5aad2aae65d56272974bb414a8352e15cef04e
RValner <valnerrobert@gmail.com>
2019-08-07 16:02:28 +0300

    Merged umrf-devel

--- scripts/temoto_setup.sh
+++ scripts/temoto_setup.sh
@@ -69,15 +69,7 @@ cd $CW_DIR/$SUBFOLDER
 
 echo -e $NL"TeMoto dependencies:"
 
-find_install_from_source rviz_plugin_manager https://github.com/UTNuclearRoboticsPublic/rviz_plugin_manager
-find_install_from_source human_msgs https://github.com/ut-ims-robotics/human_msgs
 find_install_from_source file_template_parser https://github.com/ut-ims-robotics/file_template_parser
-find_install_from_apt ros-$ROS_VERSION-moveit-ros-planning-interface
-find_install_from_apt ros-$ROS_VERSION-tf2-geometry-msgs
-
-# Install Intel TBB
-find_install_from_apt libtbb2
-find_install_from_apt libtbb-dev
 
 # Install TinyXML
 find_install_from_apt libtinyxml-dev
@@ -104,32 +96,6 @@ do
   find_install_from_source $subsys_name https://github.com/temoto-telerobotics/$subsys_name.git $SUBFOLDER
 done
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                  Export TeMoto aliases
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-if [[ -z $TEMOTO_ALIASES_SET ]]; then
-  echo -e $NL"Export TeMoto script aliases?" $RESET
-  echo -e " - temoto_status: Checks the status of TeMoto packages" $RESET
-  echo -e " - temoto_pull: updates TeMoto packages" $RESET
-  echo -e " - temoto_push: pushes local TeMoto package changes to upstream" $RESET
-  echo -e -n "(y/n)" $RESET
-  read -p " " -n 1 -r
-  echo
-
-  # If the user replied "y" or "Y"
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "" >> ~/.bashrc
-    echo "# TeMoto aliases" >> ~/.bashrc
-    echo "alias temoto_status='bash $(rospack find temoto)/scripts/temoto_status.sh'" >> ~/.bashrc
-    echo "alias temoto_pull='bash $(rospack find temoto)/scripts/temoto_pull.sh'" >> ~/.bashrc
-    echo "alias temoto_push='bash $(rospack find temoto)/scripts/temoto_push.sh'" >> ~/.bashrc
-    echo "export TEMOTO_ALIASES_SET=1" >> ~/.bashrc
-    echo -e $YELLOW"NB! The aliases are effective after you have sourced the '.bashrc' or opened a new terminal"$RESET
-  else
-    echo -n
-  fi
-fi
 
 cd $PREV_DIR
 
