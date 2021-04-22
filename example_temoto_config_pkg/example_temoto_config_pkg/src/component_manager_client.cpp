33c86d6287c7e0e61b2ce3673ee51162135d2fab
Robert <valnerrobert@gmail.com>
2021-04-22 21:13:34 +0300

    RR component pipe devel

--- example_temoto_config_pkg/example_temoto_config_pkg/src/component_manager_client.cpp
+++ example_temoto_config_pkg/example_temoto_config_pkg/src/component_manager_client.cpp
@@ -14,8 +14,8 @@ int main(int argc, char** argv)
   //ros::waitForShutdown();
 
   /*
-   * Create External Resource Manager Interface object that provides a simplified
-   * API for communicating with the External Resource Manager. The boolean "true", that's passed
+   * Create Component Manager Interface object that provides a simplified
+   * API for communicating with the Component Manager. The boolean "true", that's passed
    * to the constructor of ERM interface tells it whether it should be initialised immediately,
    * or that's done later by the user.
    */
@@ -26,20 +26,14 @@ int main(int argc, char** argv)
    */
   cmi.registerComponentStatusCallback(resourceFailureCallback);
 
-  /*
-   * ER Manager allows to invoke ROS executables, ROS launch files and other programs.
-   * The "loadRosResource" and "loadSysResource" methods return a "temoto_er_manager::LoadExtResource"
-   * object which contains the details regarding the query. This can be later used to unload the resource.
-   */
+  // ROS_INFO("Loading 2d_camera");
+  // auto responded_topics = cmi.startComponent("2d_camera");
+  // ros::Duration(15).sleep();
 
-  /*
-   * Load the Gnome calculator as an example of a regular system program. Additional
-   * arguments can also be passed as a second std::string variable.
-   */
-  ROS_INFO("Loading 2d_camera");
-  auto responded_topics = cmi.startComponent("2d_camera");
+  // ROS_INFO("Unloading 2d_camera");
+  // cmi.stopComponent("2d_camera", "", "");
+
+  ROS_INFO("Loading testpipe");
+  temoto_core::TopicContainer load_pipe_msg = cmi.startPipe("testpipe");
   ros::Duration(15).sleep();
-
-  ROS_INFO("Unloading 2d_camera");
-  cmi.stopComponent("2d_camera", "", "");
 }
---
+++ example_temoto_config_pkg/example_temoto_config_pkg/src/dummy_components/component_string_1.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+std::string string_0, string_1;
+ros::Publisher pub_0;
+ros::Publisher pub_1;
+
+// Subscriber 0 callback
+void sub0Cb(const std_msgs::String &msg)
+{
+  // std::cout << "Changing string_0 to " << msg.data << std::endl;
+  string_0 = msg.data;
+
+  // Create the messages
+  std_msgs::String msg_0;
+  msg_0.data = string_0 + " is working";
+
+  // Publish the messages
+  pub_0.publish(msg_0);
+}
+
+// Subscriber 1 callback
+void sub1Cb(const std_msgs::String &msg)
+{
+  // std::cout << "Changing string_1 to " << msg.data << std::endl;
+  string_1 = msg.data;
+
+  // Create the messages
+  std_msgs::String msg_1;
+  msg_1.data = string_1 + " is flowing";
+
+  // Publish the messages
+  pub_1.publish(msg_1);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "component_string_1_node");
+  ros::NodeHandle nh;
+
+  // Test publishers
+  pub_0 = nh.advertise<std_msgs::String>("cs_1_pub_0", 10);
+  pub_1 = nh.advertise<std_msgs::String>("cs_1_pub_1", 10);
+
+  // Test subscribers
+  ros::Subscriber sub_0 = nh.subscribe( "cs_1_sub_0", 10, sub0Cb);
+  ros::Subscriber sub_1 = nh.subscribe( "cs_1_sub_1", 10, sub1Cb);
+
+  std::cout << "component_string_1 READY\n";
+
+  ros::spin();
+
+  return 0;
+}
